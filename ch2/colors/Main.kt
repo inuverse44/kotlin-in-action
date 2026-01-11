3699b79e9506573e760b5cc55b38c685beb3ede4
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2026-01-11 22:03:48 +0900

    2026-01-11。今日もKotlin in Actionを進めて偉い！！

--- ch2/colors/Main.kt
+++ ch2/colors/Main.kt
@@ -24,10 +24,46 @@ fun getWarmthFromSensor(): String {
     }
 }
 
+// listing 2.15 いくらか非効率的
+// これはsetインスタンスを何回も作成する。大量に呼ぶのであれば、
+// 短命オブジェクトをたくさん生成するのは避けるべきである。
+// この短命オブジェクトはGCによってクリーンアップされる必要のあるものである。
+fun mix(c1: Color, c2: Color) =
+    when(setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+// 可読性は落ちるものの、mix()よりも性能は向上している
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == RED && c2 == YELLOW) ||
+        (c1 == YELLOW && c2 == RED) ->
+            ORANGE
+
+        (c1 == YELLOW && c2 == BLUE) ||
+        (c1 == BLUE && c2 == YELLOW) ->
+            GREEN
+
+        (c1 == BLUE && c2 == VIOLET) ||
+        (c1 == VIOLET && c2 == BLUE) ->
+            INDIGO
+
+        else -> throw Exception("Dirty color")
+    }
+
 fun main() {
     println(getMnemonic(Color.BLUE))
     // Battle
 
     println(getWarmthFromSensor())
     //　warm (red = 255)
+
+    println(mix(BLUE, YELLOW))
+    // GLEEN
+
+    println(mixOptimized(BLUE, YELLOW))
+    // GLEEN
 }
